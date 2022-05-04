5a977b49014b99cde1a571fb7dd0602ec0740eed
TomCallingham <21176980+TomCallingham@users.noreply.github.com>
2022-05-04 14:13:35 +0100

    Secod cosma push, gaia2 posthook test

---
+++ Notebooks/.jupy/Second_CosmaPost_HookTest.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: hydrogen
+#       format_version: '1.3'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+print("Will gaia2 make this notebook?")
+
+# %%
